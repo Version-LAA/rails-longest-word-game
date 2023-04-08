85b3ae1ca284af62f38e97901df7f672d72b8649
Latoya Alford <lat.jamison@gmail.com>
2023-04-07 22:38:52 -0400

    implemented check for word validity

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,16 +13,23 @@ class GamesController < ApplicationController
   end
 
   def score
-    @word = params[:word]
-    uri = URI.open("https://wagon-dictionary.herokuapp.com/#{@word}")
-    # @output = uri
-    @output = JSON.parse(uri.read)
-    if @output["found"] == true
-      @message = "Congratulations! #{@word} is a valid english word"
-    else
-      @message = "Sorry, but #{@word} is not a valid english word"
+    @word = params[:word].upcase
+    @board = params[:board].split
+    @words = @word.split('')
+    @status = []
+    @words.each do |c|
+      @board.include?(c) ? @status.push(true) : @status.push(false)
     end
-    @stuff = params[:board]
-    #raise
+    if @status.include?(false)
+      @message = "Word isn't on board"
+    else
+      uri = URI.open("https://wagon-dictionary.herokuapp.com/#{@word}")
+      @output = JSON.parse(uri.read)
+      if @output['found'] == true
+        @message = "Congratulations! #{@word} is a valid english word"
+      else
+        @message = "Sorry, but #{@word} is not a valid english word"
+      end
+    end
   end
 end
